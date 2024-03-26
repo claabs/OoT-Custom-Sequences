125229f410651e4b5036f7ea4bb9c72b79bc2cc5
charlocharlie <charlielaabs@gmail.com>
2024-03-25 22:30:45 -0500

    Don't extract sequences that depend on samples

--- Generate legacy format.py
+++ Generate legacy format.py
@@ -1,5 +1,26 @@
 import os
 import shutil
+import zipfile
+import re
+
+def extract_files_with_extension(zip_file, output_dir):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        files_to_extract = []
+        filelist = zip_ref.infolist()
+        for member in filelist:
+            if member.filename.endswith('.meta'):
+                with zip_ref.open(member) as file:
+                    file_content = file.read().decode('utf-8')
+                    if not re.search(r'\nZSOUND:', file_content):
+                        seq_filename = member.filename.replace('.meta', '.seq')
+                        seq_file = next(f for f in filelist if f.filename == seq_filename)
+                        if (seq_file):
+                            files_to_extract.append(member)
+                            files_to_extract.append(seq_file)
+
+        for member in files_to_extract:        
+            zip_ref.extract(member, output_dir)
+                
 
 cwd = os.getcwd()
 
@@ -19,8 +40,8 @@ for root, dirs, files in os.walk(source_dir):
             file_path = os.path.join(root, filename)
 
             # Get group dir path (e.g. `Banjo-Kazooie Series/Banjo-Kazooie`)
-            relative_group_path = os.path.relpath(file_path, source_dir)
+            relative_group_path = os.path.relpath(os.path.dirname(file_path), source_dir)
 
             # Append group path to destinarion dir
             sequence_output_dir = os.path.join(destination_dir, relative_group_path)
-            shutil.unpack_archive(file_path, sequence_output_dir, 'zip')
+            extract_files_with_extension(file_path, sequence_output_dir)
